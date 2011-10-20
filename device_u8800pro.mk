e5c3ed0d4bf92b03bbfd8c564b648ed6f3481f0d
genokolar <genokolar@gmail.com>
2011-10-20 17:00:42 +0800

    first commit

---
+++ device_u8800pro.mk
@@ -0,0 +1,177 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for sapphire hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+DEVICE_PACKAGE_OVERLAYS := device/huawei/u8800pro/overlay
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := huawei_u8800pro
+PRODUCT_DEVICE := u8800pro
+PRODUCT_MODEL := huawei u8800pro
+# Geno ADD
+#PRODUCT_LOCALES := zh_CN zh_TW en_US
+
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni \
+    Gallery3d \
+    SpareParts \
+    Term \
+    overlay.default \
+    gps.u8800pro \
+    lights.u8800pro \
+    gralloc.msm7x30 \
+    overlay.default \
+    libOmxCore \
+    libOmxVidEnc \
+    Torch \
+    dexpreopt \
+
+# proprietary side of the device
+$(call inherit-product-if-exists, vendor/huawei/u8800pro/u8800pro-vendor.mk)
+
+DISABLE_DEXPREOPT := false
+
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/qwerty.kl:system/usr/keylayout/qwerty.kl
+
+# fstab
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/vold.fstab:system/etc/vold.fstab
+
+# enhanced.conf
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/enhanced.conf:system/etc/enhanced.conf
+
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/huawei/u8800pro/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/huawei/u8800pro/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/huawei/u8800pro/media_profiles.xml:system/etc/media_profiles.xml
+
+# vidc firmware
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/firmware/vidc_720p_command_control.fw:/system/etc/firmware/vidc_720p_command_control.fw \
+    device/huawei/u8800pro/firmware/vidc_720p_h263_dec_mc.fw:/system/etc/firmware/vidc_720p_h263_dec_mc.fw \
+    device/huawei/u8800pro/firmware/vidc_720p_h264_dec_mc.fw:/system/etc/firmware/vidc_720p_h264_dec_mc.fw \
+    device/huawei/u8800pro/firmware/vidc_720p_h264_enc_mc.fw:/system/etc/firmware/vidc_720p_h264_enc_mc.fw \
+    device/huawei/u8800pro/firmware/vidc_720p_mp4_dec_mc.fw:/system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
+    device/huawei/u8800pro/firmware/vidc_720p_mp4_enc_mc.fw:/system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
+    device/huawei/u8800pro/firmware/vidc_720p_vc1_dec_mc.fw:/system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
+
+# Install the features available on this device.
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+
+#WIFI Modules
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/prebuilt/dhd.ko:/system/wifi/dhd.ko \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.android.dateformat=dd-MM-yyyy \
+    ro.build.baseband_version=129005 \
+    ro.telephony.default_network=0 \
+    ro.telephony.call_ring.multiple=false \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    rild.libpath=/system/lib/libril-qc-1.so \
+    rild.libargs=-d /dev/smd0 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15 \
+    ro.com.android.dataroaming=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240 \
+    persist.sys.language=zh \
+    persist.sys.country=CN \
+    persist.sys.timezone=Asia/Shanghai
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.hwfeature_slavecamera=yes \
+    ro.config.hwcameraset=63 \
+    hw.secondary_mic=0 \
+    ro.config.hw_addsettingsdbex=1 \
+    ro.config.hw_gcf_mms=true \
+    ro.config.wifi_chip_is_bcm=false \
+    ro.config.hwft_PNN_function=true \
+    persist.cust.tel.adapt=1 \
+    persist.cust.tel.eons=1 \
+    ro.config.hwfeature_gps_test=0 \
+    ro.config.hwfeature_ecc=true \
+    ro.config.lowbattery_shutdown=1 \
+    ro.config.hwfeature_wakeupkey=1 \
+    ro.config.endkeybehavior=true \
+    ro.config.hw_menu_unlockscreen=false \
+    ro.media.enc.lprof.duration=30 \
+    ro.config.PicMaxSize=5mp \
+    ro.config.hw_temperature_warn=true \
+    ro.config.hw_clocksetting=0 \
+    ro.additionalmounts=/HWUserData \
+    ro.vold.switchablepair=/mnt/sdcard,/HWUserData \
+    ro.vold.umsdirtyratio=20 \
+
+# u8800pro uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# This should not be needed but on-screen keyboard uses the wrong density without it.
+PRODUCT_PROPERTY_OVERRIDES += \
+    qemu.sf.lcd_density=240 
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.locationfeatures=1 \
+    ro.setupwizard.enable_bypass=1 \
+    ro.media.dec.jpeg.memcap=20000000 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.heapsize=32m \
+    dalvik.vm.execution-mode=int:jit \
+    dalvik.vm.dexopt-data-only=1 \
+    ro.opengles.version=131072  \
+    ro.compcache.default=0
+
+
+# addon by Geno
+$(call inherit-product-if-exists, addon/addon.mk)
+$(call inherit-product-if-exists, device/huawei/u8800pro/wifi/wifi.mk)
+
---
+++ wifi/wifi.mk
@@ -0,0 +1,11 @@
+# Audio
+PRODUCT_COPY_FILES += \
+    device/huawei/u8800pro/wifi/bcm_loadecho.sh:system/wifi/bcm_loadecho.sh \
+    device/huawei/u8800pro/wifi/bcm_loadipf.sh:system/wifi/bcm_loadipf.sh \
+    device/huawei/u8800pro/wifi/connectap.sh:system/wifi/connectap.sh \
+    device/huawei/u8800pro/wifi/iwconfig:system/wifi/iwconfig \
+    device/huawei/u8800pro/wifi/iwlist:system/wifi/iwlist \
+    device/huawei/u8800pro/wifi/iwpriv:system/wifi/iwpriv \
+    device/huawei/u8800pro/wifi/nvram.txt:system/wifi/nvram.txt \
+    device/huawei/u8800pro/wifi/udp_server:system/wifi/udp_server \
+
